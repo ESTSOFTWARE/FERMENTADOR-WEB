941686124a155e4c7f6c239e2d6b11150e9fc649
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2026-06-30 12:07:54 -0600

    feat: footer

--- src/features/landing/presentation/components/Footer.tsx
+++ src/features/landing/presentation/components/Footer.tsx
@@ -5,27 +5,27 @@ import { useUserAuth } from "../../../../core/hooks/userAuth";
 
 const footerLinks: Record<string, { label: string; href: string; target?: string }[]> = {
   Páginas: [
-    { label: "Inicio", href: "#" },
-    { label: "Características", href: "#features" },
-    //{ label: "Precios", href: "#pricing" },
-    { label: "¿Como funciona?", href: "#how-it-works" },
-    { label: "Blog", href: "#blog" },
-    { label: "Equipo", href: "#team" },
+    { label: "Inicio",               href: "#" },
+    { label: "App móvil",            href: "#app" },
+    { label: "Objetivos",            href: "#objetivos" },
+    { label: "Negocio",              href: "#negocio" },
+    { label: "Equipo",               href: "#team" },
+    { label: "Experiencias",         href: "/experiences" },
   ],
   Redes: [
-    { label: "Facebook", href: "https://www.facebook.com/share/17AH5RJtvN/", target: "_blank" },
-    { label: "Instagram", href: "https://www.instagram.com/nich_ka_space?utm_source=qr&igsh=MTJvZHc4bmlpZ3k5", target: "_blank" },
-    { label: "Tiktok", href: "https://www.tiktok.com/@nich_ka_space?_r=1&_t=ZS-95hawcvOw6R", target: "_blank" },
-    { label: "LinkedIn", href: "https://www.linkedin.com/company/nich-k%C3%A1/about/", target: "_blank" },
+    { label: "Facebook",             href: "https://www.facebook.com/share/17AH5RJtvN/", target: "_blank" },
+    { label: "Instagram",            href: "https://www.instagram.com/nich_ka_space?utm_source=qr&igsh=MTJvZHc4bmlpZ3k5", target: "_blank" },
+    { label: "Tiktok",               href: "https://www.tiktok.com/@nich_ka_space?_r=1&_t=ZS-95hawcvOw6R", target: "_blank" },
+    { label: "LinkedIn",             href: "https://www.linkedin.com/company/nich-k%C3%A1/about/", target: "_blank" },
   ],
   Legal: [
-    { label: "Privacidad", href: "/privacy" },
-    { label: "Términos de uso", href: "/terms" },
-    { label: "Cookies", href: "/cookies" },
+    { label: "Privacidad",           href: "/privacy" },
+    { label: "Términos de uso",      href: "/terms" },
+    { label: "Cookies",              href: "/cookies" },
   ],
   Cuenta: [
-    { label: "Registrarse", href: "/register" },
-    { label: "Iniciar sesión", href: "/login" },
+    { label: "Registrarse",          href: "/register" },
+    { label: "Iniciar sesión",       href: "/login" },
     { label: "Olvidé mi contraseña", href: "/forgot-password" },
   ],
 };
